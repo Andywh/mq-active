367def8e7578ea6c18b623fe2678e554ae9fb004
Andywh <529254699@qq.com>
2020-10-08 17:32:17 +0800

    init

--- src/main/java/com/joy/activemq/Sender.java
+++ src/main/java/com/joy/activemq/Sender.java
@@ -1,13 +1,32 @@
 package com.joy.activemq;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.camel.component.ActiveMQConfiguration;
+
 /**
  * Created by Ai Lun on 2020-10-08.
  */
 public class Sender {
 
-    // 1.获取连接工厂
+    public static void main(String[] args) {
+        // 1. 获取连接工厂
+        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory();
 
-    // 2.获取一个向ActiveMQ的连接
+        // 2. 获取一个向ActiveMQ的连接
 
-    
+        // 3. 获取 session
+
+        // 4. 找目的地，获取 destination.
+        // 消费端，也会从这个目的地取消息
+
+        // 5. 向目的地写入消息
+
+        // 6. 关闭连接
+
+
+
+    }
+
+
+
 }
